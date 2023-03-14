b4dcfd6ea5baa207c235f613a99630c398b32ef3
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-03-14 12:12:17 -0300

    Subiu m

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,4 +1,12 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProjects.module.css'
 function NewProject(){
-    return <h1>Novo Projeto</h1>
+    return (
+        <div className={styles.newproject_container}>
+        <h1>Criar Projeto</h1>
+        <p>Crie seu projeto para adicionar os serviços</p>
+        <ProjectForm/>
+        </div>
+    )
 }
 export default NewProject
