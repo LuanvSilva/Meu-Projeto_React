96b94193cc9651f04f6e9eb0cea7712d1b0fbb32
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-03-27 12:47:43 -0300

    ATUALIZA

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,27 +1,37 @@
-import { useNavigate} from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
+
 import ProjectForm from '../project/ProjectForm'
+
 import styles from './NewProjects.module.css'
-function NewProject(){
-    const navigate = useNavigate()
-    function createPost(project){
-        project.cost = 0
-        project.services = []
-         fetch('http://localhost:5000/projects',{
-            method:'POST',
-            Headers: {
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify(project),
 
-        }).then((resp) => resp.json()).then((date) => navigate('/projects', { message: 'Projeto criado com suceso'}))
-    }
+function NewProject() {
+  const navigate = useNavigate()
 
-    return ( 
-        <div className={styles.newproject_container}>
-        <h1>Criar Projeto</h1>
-        <p>Crie seu projeto para adicionar os serviços<br></br></p>
-        <ProjectForm handleSubmit={createPost} btnText="Criar Projeto"/>
-        </div>
-    )
+  function createPost(project) {
+    // initialize cost and services
+    project.cost = 0
+    project.services = []
+
+    fetch('http://localhost:5000/projects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        navigate('/projects', { message: 'Projeto criado com sucesso!' })
+      })
+  }
+
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+    </div>
+  )
 }
+
 export default NewProject
