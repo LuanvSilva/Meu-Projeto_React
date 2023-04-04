98687396c1904ec682c2d78ddba56722a43ca196
Luan Silva <109970489+LuanvSilva@users.noreply.github.com>
2023-04-03 21:39:06 -0300

    Subiu

--- src/components/layout/Message.js
+++ src/components/layout/Message.js
@@ -3,9 +3,28 @@ import {useState, useEffect} from 'react'
 
 function Message({type,msg}){
     const [visible, setVisible] = useState(false)
+   
+   useEffect(() =>{
+
+    if(!msg){
+        setVisible(false)
+        return
+    }
+    setVisible(true)
+    const timer = setTimeout(() => {
+        setVisible(false)
+    }, 3000)
+    return () => clearTimeout(timer)
+
+   },[msg])
+   
     return (
+        <>
+        {visible && (
+        <div className={`${styles.message} ${styles[type]}`}>{msg}</div>  
         
-       <div className={`${styles.message} ${styles[type]}`}>{msg}</div>  
+        )}
+       </>
     )
 }
 export default Message  
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,10 +1,16 @@
+import {useLocation} from 'react-router-dom'
 import Message from "../layout/Message"
 
 function Projects(){
+const location = useLocation()
+let message = ''
+if(location.state){
+    message = location.state.message
+}
     return (
         <div>
             <h1>Projetos/</h1>
-            <Message msg='Mensagem Dinamica' type="error"/>
+            {message && <Message type="sucess" msg={message}/>}
         </div>
     )
 }
